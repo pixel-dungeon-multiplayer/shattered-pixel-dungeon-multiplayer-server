43cd76c9cb43d99206b17bef26398325ca891c7d
Nikita22007 <shaposhnikov.nikita22007@yandex.ru>
2026-06-17 16:20:02 +0300

    WndQuest serializer

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/network/serializers/windows/wndoptions/WndQuestSerializer.java
@@ -0,0 +1,28 @@
+package com.shatteredpixel.shatteredpixeldungeon.network.serializers.windows.wndoptions;
+
+import com.shatteredpixel.shatteredpixeldungeon.network.serializers.SerializationContext;
+import com.shatteredpixel.shatteredpixeldungeon.network.serializers.windows.WindowSerializer;
+import com.shatteredpixel.shatteredpixeldungeon.windows.WndQuest;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.json.JSONObject;
+
+public class WndQuestSerializer extends WindowSerializer<WndQuest> {
+
+    @Override
+    protected @NotNull String type() {
+        return "wnd_option";
+    }
+
+    @Override
+    protected @Nullable JSONObject args(@NotNull WndQuest obj, @NotNull SerializationContext ctx, @NotNull String profile) {
+        WndOptionContract contract = new WndOptionContract();
+        contract.titleText = obj.charName();
+        contract.message = obj.text();
+        contract.titleIcon = WndOptionContract.TitleIcon.charSprite(obj.spriteName());
+        contract.layout = WndOptionContract.Layout.titledMessage();
+        return contract.toJson(ctx, profile);
+    }
+}
+
+

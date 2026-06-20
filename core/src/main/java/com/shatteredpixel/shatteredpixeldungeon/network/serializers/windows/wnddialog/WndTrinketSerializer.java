49b25c271e937141c1a13119cf173e2d29cff0df
Nikita22007 <game.nikita22007@yandex.ru>
2026-06-20 21:52:58 +0300

    Refactor WndTrinket and its RewardWindow to use standard wnd_dialog serialization

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/network/serializers/windows/wnddialog/WndTrinketSerializer.java
@@ -0,0 +1,24 @@
+package com.shatteredpixel.shatteredpixeldungeon.network.serializers.windows.wnddialog;
+
+import com.shatteredpixel.shatteredpixeldungeon.items.trinkets.Trinket;
+import com.shatteredpixel.shatteredpixeldungeon.items.trinkets.TrinketCatalyst;
+import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
+import com.shatteredpixel.shatteredpixeldungeon.network.serializers.SerializationContext;
+import com.shatteredpixel.shatteredpixeldungeon.network.serializers.ui.ImageIcon;
+import org.jetbrains.annotations.NotNull;
+
+public class WndTrinketSerializer extends WndDialogSerializer<TrinketCatalyst.WndTrinket> {
+
+    @Override
+    protected @NotNull WndDialogContract getContract(@NotNull TrinketCatalyst.WndTrinket obj, @NotNull SerializationContext ctx, @NotNull String profile) {
+        WndDialogContract contract = new WndDialogContract();
+        contract.titleText = Messages.titleCase(Messages.get(TrinketCatalyst.class, "window_title"));
+        contract.message = Messages.get(TrinketCatalyst.class, "window_text");
+        contract.titleIcon = ImageIcon.itemSprite(obj.catalyst().image(), null);
+        contract.layout = WndDialogContract.Layout.titledMessage();
+        for (Trinket trinket : obj.rolledTrinkets()) {
+            contract.itemSlots.add(new WndDialogContract.ItemSlot(trinket, true, true));
+        }
+        return contract;
+    }
+}

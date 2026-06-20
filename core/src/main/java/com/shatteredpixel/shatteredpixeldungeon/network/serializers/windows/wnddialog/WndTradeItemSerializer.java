cc5437be9cabac35fd33bc013ffc92df9ebcd675
Nikita22007 <game.nikita22007@yandex.ru>
2026-06-20 21:42:09 +0300

    Refactor WndTradeItem and WndTradeItemSerializer to use standard WndDialog serialization

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/network/serializers/windows/wnddialog/WndTradeItemSerializer.java
@@ -0,0 +1,7 @@
+package com.shatteredpixel.shatteredpixeldungeon.network.serializers.windows.wnddialog;
+
+import com.shatteredpixel.shatteredpixeldungeon.network.serializers.windows.wnddialog.wndoptions.WndInfoItemSerializer;
+import com.shatteredpixel.shatteredpixeldungeon.windows.WndTradeItem;
+
+public class WndTradeItemSerializer extends WndInfoItemSerializer<WndTradeItem> {
+}

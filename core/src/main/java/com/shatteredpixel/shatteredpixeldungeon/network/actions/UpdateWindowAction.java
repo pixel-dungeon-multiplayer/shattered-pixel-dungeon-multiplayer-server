f0a1f836cf0e47f19e2e462ea35957ada220da1c
Nikita22007 <game.nikita22007@yandex.ru>
2026-06-20 20:11:50 +0300

    Wnd ifon cell serializer

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/network/actions/UpdateWindowAction.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/network/actions/UpdateWindowAction.java
@@ -1,12 +1,14 @@
 package com.shatteredpixel.shatteredpixeldungeon.network.actions;
 
 import com.shatteredpixel.shatteredpixeldungeon.ui.Window;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public class UpdateWindowAction implements LiveStateNetworkAction {
 
     public final @NotNull Window window;
 
+    @Contract(pure = true)
     public UpdateWindowAction(@NotNull Window window) {
         this.window = window;
     }
